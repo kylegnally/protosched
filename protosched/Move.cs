23e55d0730483873055033c691692451223290a6
Kyle G. Nally <kylegnally@hotmail.com>
2019-05-03 09:45:19 -0400

    Continue writing Move code.

--- protosched/Move.cs
+++ protosched/Move.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace protosched
 {
-    class Move
+    abstract class Move : IMove
     {
         private Equipment EquipmentName;
         private string BuildingName;
@@ -17,4 +17,27 @@ namespace protosched
         private DateTime NeededFrom;
         private DateTime NeededTo;
     }
+
+    protected Move(DateTime moveFrom,
+    DateTime moveto)
+    {
+
+    }
+
+    public int CalculateOrder(object move)
+    {
+        if (move is IMove otherMove)
+            return this.TotalCost.CompareTo(otherMove.TotalCost);
+        else
+            throw new ArgumentException("Object is not a Move");
+    }
+
+    ///// <summary>
+    ///// returns the basic droid information when called from any child class.
+    ///// </summary>
+    ///// <returns></returns>
+    //public override string ToString()
+    //{
+    //    return TotalCost.ToString("C") + " " + Color + " " + Material + " " + Name + " ";
+    //}
 }
