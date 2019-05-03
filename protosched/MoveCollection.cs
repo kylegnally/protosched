e59481adf7a38fee1b37635013771d812c42233b
Kyle G. Nally <kylegnally@hotmail.com>
2019-05-03 11:48:59 -0400

    Begin adding code to MoveCollection.

--- protosched/MoveCollection.cs
+++ protosched/MoveCollection.cs
@@ -11,10 +11,19 @@ namespace protosched
         private IMove[] moves;
         private int collectionPosition;
 
+        // generic stacks for each move type
         GenericStack<IMove> aWowStack = new GenericStack<IMove>();
         GenericStack<IMove> aVisStack = new GenericStack<IMove>();
         GenericStack<IMove> aBoardStack = new GenericStack<IMove>();
         GenericStack<IMove> aMiscStack = new GenericStack<IMove>();
 
+        // generic queue for all moves
+        GenericQueue<IMove> movesQueue = new GenericQueue<IMove>();
+
+        public MoveCollection()
+        {
+            moves = new IMove[100];
+            collectionPosition = 0;
+        }
     }
 }
