9a77c65cc18462598a296df4ac90bc278e118bf9
Kyle G. Nally <kylegnally@hotmail.com>
2019-05-03 08:20:02 -0400

    Initial commit. Create some classes. Begin writing interfaces.

---
+++ protosched/IMove.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protosched
+{
+    interface IMove : IComparable
+    {
+        // method to calculate the order this move is in the list (should be elsewhere?)
+        void CalculateOrder();
+
+        // property to hold this move's order
+        int GetOrder { get; set; }
+
+    }
+}
---
+++ protosched/IMoveCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protosched
+{
+    interface IMoveCollection
+    {
+        void Add(Move move);
+        void Remove(Move move);
+    }
+}
