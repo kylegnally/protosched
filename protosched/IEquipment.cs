8dbda9b3aac7ccac5c5f66c090f95ae53e678854
Kyle G. Nally <kylegnally@hotmail.com>
2019-05-05 09:49:31 -0400

    Begin writing code in IEquipment.

--- protosched/IEquipment.cs
+++ protosched/IEquipment.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace protosched
 {
-    interface IEquipment
+    interface IEquipment : IComparable
     {
+        string Name { get; set; }
+        string CurrentRoom { get; set; }
+
+        DateTime[] NeededRange { get; set; }
+
+
+        void CompareTo();
     }
 }
