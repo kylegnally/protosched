27a437ebabd83ab26d2e32095b40c9a3a32ffe93
Kyle G. Nally <kylegnally@hotmail.com>
2019-05-09 08:25:42 -0400

    Create Session class.

---
+++ protosched/Session.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protosched
+{
+    class Session
+    {
+        private DateTime[] range;
+        private int lengthInMinutes;
+
+        public DateTime[] SessionTimes { get; set; }
+        public TimeSpan SessionLength { get; set; }
+
+        public Session(DateTime startTime, DateTime endTime)
+        {
+            range = new DateTime[2];
+            range[0] = startTime;
+            range[1] = endTime;
+            SessionTimes = range;
+            SessionLength = endTime - startTime;
+        }
+    }
+}
